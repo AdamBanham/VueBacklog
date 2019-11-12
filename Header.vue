c7270054011642a471abfac3ce0548c4adcbc31d
Adam Peter Banham <n7176546@qut.edu.au>
2019-11-12 10:54:05 +1000

    added awesome header to use

---
+++ Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+        <p>
+            This is a header!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    Name: "Header"
+}
+</script>
+
+<style scoped>
+
+.header{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    width:100%;
+    height: 50px;
+    background-color: rgb(32, 30, 30)
+}
+p{
+    width: 100%;
+    height: 100%;
+    align-self: auto;
+    text-align: center;
+    color: whitesmoke;
+}
+</style>
